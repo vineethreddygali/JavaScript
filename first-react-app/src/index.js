9ef184e82faf7d45d814e78cf8fb2b5bcdde7ef6
Vineeth Reddy Gali <36160523+vineethreddygali@users.noreply.github.com>
2021-03-18 00:33:22 -0400

    code update 2

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -3,34 +3,47 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-function LoginForm(props) {
-  const nameEl = React.useRef(null);
-  const passwordEl = React.useRef(null);
-  const rememberMeEl = React.useRef(null);
+function DetailsPage(props) {
+  const firstName = React.useRef(null);
+  const lastName = React.useRef(null);
+  const emailId = React.useRef(null);
+  const phone = React.useRef(null);
+  const age = React.useRef(null);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const formData = {
-      username: nameEl.current.value,
-      password: passwordEl.current.value,
-      rememberMe: rememberMeEl.current.checked,
+  
+
+  const handleSubmit = a => {
+    a.preventDefault();
+    const details = {
+      firstname: firstName.current.value,
+      lastname: lastName.current.value,
+      Email: emailId.current.value,
+      Phone: phone.current.value,
+      Age: age.current.value,
+
     }
     // it would be a more completely correct REACT approach
     // to make a JSX component to render formData out to a selector on the HTML
-    console.log(formData);
+    console.log(details);
   };
 
   return (
      <form onSubmit={handleSubmit}>
-       <input type="text" placeholder="username" ref={nameEl} />
-       <input type="password" placeholder="password" ref={passwordEl} />
-       <label>
-         <input type="checkbox" ref={rememberMeEl} />
-         Remember me
-       </label>
-       <button type="submit" className="myButton">Login</button>
+       Firstname: <input type="text" ref={firstName}/>
+       <br></br>
+       Lastname: <input type="text" ref={lastName}/>
+       <br></br>
+       Email: <input type="text" ref={emailId}/>
+       <br></br>
+       Phone: <input type="number" ref={phone}/>
+       <br></br>
+       Age: <input type="number" ref={age}/>
+       <br></br>
+      
+       <button type="submit" className="button">Submit</button>
+       <button type="reset">Reset</button>
      </form>
    );
 }
 
-ReactDOM.render(<div><LoginForm /></div>, document.getElementById("root1"));
+ReactDOM.render(<div><DetailsPage /></div>, document.getElementById("root1"));
