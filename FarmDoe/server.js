d8227444746a5aa1bea8657f3d73ade4318ba87b
Vineeth Reddy Gali <36160523+vineethreddygali@users.noreply.github.com>
2021-03-27 15:37:16 -0400

    code update 3

--- FarmDoe/server.js
+++ FarmDoe/server.js
@@ -5,16 +5,41 @@ var db = new sqlite3.Database('VineethReddyGali-C0777252.db');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  
+ // res.write('Starting Database pull \n');
+
   db.all("SELECT * FROM Fields", function(err, rows) {
+    res.write('Fields are:\n');
     rows.forEach(function(row) {
         res.write(row.fieldName+'\n');
     })
 
-    
 
+
+//res.end('done processing ');
   })
+  db.all("SELECT * FROM Veggies", function(err, rows) {
+    res.write('\nField name:Crop:Quantity\n');
+    rows.forEach(function(row) {
+        res.write(row.fieldName+':'+row.Crop+':'+row.Quantity+'\n');
+    })
+
+
+
+//res.end('');
+  })
+
+  db.all("SELECT * FROM veggiePrice", function(err, rows) {
+    res.write('\nVeggie Name:Price\n');
+    rows.forEach(function(row) {
+        res.write(row.veggieName+':'+row.Price+'\n');
+    })
+
+
+
+res.end('');
+  })
+
 db.close();
 
 
-}).listen(8080);
+}).listen(8080); 
